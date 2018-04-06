33a5ba792a79c79fa17467e7b1147f9ca28be204
Vi <1149443+mistodon@users.noreply.github.com>
2018-04-06 13:23:29 +0100

    Add tweakable config in debug mode

---
+++ src/config.rs
@@ -0,0 +1,23 @@
+use std::borrow::Cow;
+pub use gen_config::{CONFIG, Config};
+
+pub fn load_config() -> Cow<'static, Config> {
+    #[cfg(debug_assertions)]
+    {
+        use std::fs::File;
+        use std::io::Read;
+        use toml;
+
+        let mut file = File::open("assets/config.toml").unwrap();
+
+        let mut buffer = String::new();
+        file.read_to_string(&mut buffer).unwrap();
+
+        Cow::Owned(toml::from_str(&buffer).unwrap())
+    }
+
+    #[cfg(not(debug_assertions))]
+    {
+        Cow::Borrowed(&CONFIG)
+    }
+}
